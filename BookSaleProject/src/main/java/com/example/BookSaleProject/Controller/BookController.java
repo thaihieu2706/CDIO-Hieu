9905bff23da40275180cfda0b14cd503aead90cc
Hoàng Việt <vietnguyen0312@gmail.com>
2024-04-21 18:36:02 +0700

    commit fix html

--- BookSaleProject/src/main/java/com/example/BookSaleProject/Controller/BookController.java
+++ BookSaleProject/src/main/java/com/example/BookSaleProject/Controller/BookController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,5 +37,11 @@ public class BookController {
         System.out.println("hello");
         model.addAttribute("book", book);
         return "productDetail";
+    }
+
+    @PostMapping(value = {"/updateBook"})
+    public String updateBook(Model model, @ModelAttribute("bookFinded")Book book){
+        bookService.update(book);
+        return "";
     }
 }
---
+++ BookSaleProject/src/main/resources/static/js/reUse.js
@@ -0,0 +1,18 @@
+function showSearchBar() {
+    var searchBar = document.getElementById("search-bar");
+    var displayButton = document.getElementById("dropbtn");
+    var all = document.getElementById("1");
+    if (searchBar.style.display === "none") {
+        searchBar.style.display = "block";
+        displayButton.style.display = "none"
+        all.style.display = "none"
+    }
+}
+function cancelBar() {
+    var searchBar = document.getElementById("search-bar");
+    if (searchBar.style.display === "block") {
+        searchBar.style.display = "none";
+        displayButton.style.display = "block"
+        all.style.display = "block"
+    }
+}
